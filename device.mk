18dbca532581b68c8c3548220f49255d9e658158
aarish124 <aarishkhanak123@gmail.com>
2020-03-19 01:58:17 +0100

    Revert "Revert "santoni: remove -qti suffix from power""
    This reverts commit 292f71dc1da2ec7167030286b1380cddd624af02.

--- device.mk
+++ device.mk
@@ -266,11 +266,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Permissions
-
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
